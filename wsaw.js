82dede5f07a161758c7fc51c7dee7f84d94cf7b0
wasim wasim <32330443+wasimwasim@users.noreply.github.com>
2018-05-14 06:32:06 +0300

    Update wsaw.js

--- wsaw.js
+++ wsaw.js
@@ -1,20 +1,6 @@
-var btn = document.querySelector('.button');
-var buttonWrapper = document.querySelectorAll('.button-wrapper');
+var openLoginRight = document.querySelector('.h1');
+var loginWrapper = document.querySelector('.login-wrapper');
 
-btn.addEventListener('click', function(){
-  
-  if(btn.classList.contains('animation')) {
-    btn.classList.remove('animation');
-  } else {
-    btn.classList.add('animation');
-  }
-  
-  for (i = 0; i < buttonWrapper.length; i++ ) {
-    if(buttonWrapper[i].classList.contains('animation')){
-      buttonWrapper[i].classList.remove('animation');   
-    } else {
-      buttonWrapper[i].classList.add('animation');
-    }  
-  }
-  
+openLoginRight.addEventListener('click', function(){
+  loginWrapper.classList.toggle('open'); 
 });
